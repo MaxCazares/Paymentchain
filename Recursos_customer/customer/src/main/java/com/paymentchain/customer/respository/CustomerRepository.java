125c377176b99f3d5e731355a627e7b37cdab6c6
Max Cazares <max.cazaresm@gmail.com>
2024-08-23 16:36:03 -0600

    Terminamos la sección 2: la API customer esta lista

--- Recursos_customer/customer/src/main/java/com/paymentchain/customer/respository/CustomerRepository.java
+++ Recursos_customer/customer/src/main/java/com/paymentchain/customer/respository/CustomerRepository.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Repository.java to edit this template
  */
-package com.paymentchain.customer.repository;
+package com.paymentchain.customer.respository;
 
 import com.paymentchain.customer.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
- * @author Proyecto01TICs
+ * @author sotobotero
  */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    
 }
--- costumer/src/test/java/com/paymentchain/customer/CustomerApplicationTests.java
+++ costumer/src/test/java/com/paymentchain/customer/CustomerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.paymentchain.costumer;
+package com.paymentchain.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CostumerApplicationTests {
+class CustomerApplicationTests {
 
 	@Test
 	void contextLoads() {
